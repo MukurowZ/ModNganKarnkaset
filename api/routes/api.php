533350fffad4d5fd4afe2c6eaee244e525b289a9
MukurowZ <zoz2542@gmail.com>
2019-07-23 00:45:04 +0700

    [EDIT] api routing from Plural to Sigular

--- api/routes/api.php
+++ api/routes/api.php
@@ -16,3 +16,10 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::apiResource('content','ContentController');
+Route::apiResource('event','EventController');
+Route::apiResource('activity','ActivityController');
+Route::apiResource('user','UserController');
+Route::apiResource('department','DepartmentController');
