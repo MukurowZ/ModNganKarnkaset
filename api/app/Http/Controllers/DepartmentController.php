7075159fcc2ee0efd07934825c9a2765d3090af3
MukurowZ <zoz2542@gmail.com>
2019-07-23 00:42:31 +0700

    [EDIT] Departments to Department

---
+++ api/app/Http/Controllers/DepartmentController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Model\Department;
+use App\Http\Requests\DepartmentRequest;
+
+class DepartmentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Department::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(DepartmentRequest $request)
+    {
+        $validated = $request->validated();
+        return Department::create($validated);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Model\Department  $departments
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Department $department)
+    {
+        return $department;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Model\Department  $departments
+     * @return \Illuminate\Http\Response
+     */
+    public function update(DepartmentRequest $request, Department $department)
+    {
+        $validated = $request->validated();
+        $department->update($validated);
+        return $department;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Model\Department  $departments
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Department $department)
+    {
+        $departments->delete();
+        return response('', 204);//
+    }
+}
---
+++ api/app/Model/Department.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Department extends Model
+{
+    protected $fillable = [
+        'id','name'
+    ];
+
+    protected $table = "department";
+    protected $appends = [
+
+    ];
+
+    public $incrementing = true;
+    public $timestamps = false;
+
+    protected $hidden = [
+
+    ];
+
+}
