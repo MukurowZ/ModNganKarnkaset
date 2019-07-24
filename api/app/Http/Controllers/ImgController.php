49c5a5ceff398d73bd311c8e5f67d5c6155d7b40
MukurowZ <zoz2542@gmail.com>
2019-07-24 23:40:06 +0700

    [ADD] add Img basic CRUD

---
+++ api/app/Http/Controllers/ImgController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Model\Img;
+use App\Http\Requests\ImgRequest;
+
+class ImgController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Img::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(ImgRequest $request)
+    {
+        $validated = $request->validated();
+        return Img::create($validated);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Model\Img  $img
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Img $img)
+    {
+        return $img;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Model\Img  $img
+     * @return \Illuminate\Http\Response
+     */
+    public function update(ImgRequest $request, Img $img)
+    {
+        $validated = $request->validated();
+        $img->update($validated);
+        return $img;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Model\Img  $img
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Img $img)
+    {
+        $img->delete();
+        return response('', 204);
+    }
+}
