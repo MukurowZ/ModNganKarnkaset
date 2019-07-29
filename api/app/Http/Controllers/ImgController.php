536e9639d46cf92c7fd8176ee6ede0cd6a6cc5b3
MukurowZ <zoz2542@gmail.com>
2019-07-30 05:25:06 +0700

    [ADD] Multiple img upload to ImgController

--- api/app/Http/Controllers/ImgController.php
+++ api/app/Http/Controllers/ImgController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Model\Img;
 use App\Http\Requests\ImgRequest;
+use Illuminate\Support\Facades\Input;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Http\Testing\File;
 
 class ImgController extends Controller
 {
@@ -77,4 +80,34 @@ class ImgController extends Controller
         $search = $id['id'];
         return Img::where('img_set_id', $search)->first();
     }
+
+
+    public function post_upload()
+    {
+		$input = Input::all();
+		$rules = array(
+		    'file' => 'image|max:3000',
+		);
+        dd('dsadas');
+		$validation = Validator::make($input, $rules);
+
+		if ($validation->fails())
+		{
+			return Response::make($validation->errors->first(), 400);
+		}
+
+		$file = Input::file('file');
+
+        $extension = File::extension($file['name']);
+        $directory = path('public').'uploads/'.sha1(time());
+        $filename = sha1(time().time()).".{$extension}";
+
+        $upload_success = Input::upload('file', $directory, $filename);
+
+        if( $upload_success ) {
+        	return Response::json('success', 200);
+        } else {
+        	return Response::json('error', 400);
+        }
+	}
 }
