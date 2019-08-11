b393b22de887299cc83b308d85f20dcbc21beb1e
MukurowZ <zoz2542@gmail.com>
2019-08-12 04:20:42 +0700

    [EDIT] reduce redundance

--- api/resources/views/create-event.blade.php
+++ api/resources/views/create-event.blade.php
@@ -1,78 +1,18 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
-        <script src="{{ asset('js/app.js') }}"></script>
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-        <title>เพิ่มกิจกรรม</title>
+@extends('core')
+@section('title','เพิ่มกิจกรรม')
+@section('navbar')
+@parent
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
-        <!-- Styles -->
-        <link rel="stylesheet" href="/css/app.css">
-        <style>
+@endsection
+@section('content')
+<body>
+<div id="app" class="d-inline-flex">
+    <create-event-component></create-event-component>
+</div>
 
-        </style>
-    </head>
-    <body>
-    <div id="app">
-        <create-event-component></create-event-component>
-    </div>
+@include('core_upload')
 
-    <button type="button" class="btn btn-primary" style="margin-left: 10%" data-toggle="modal" data-target="#exampleModal">
-        เพิ่มรูปภาพ
-    </button>
-
-    <!-- Modal -->
-    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-        <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalLabel">อัปโหลดรูปด้านล่าง</h5>
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-            </button>
-        </div>
-        <div class="modal-body">
-        <form class="form-group" method="post" action="/api/img/upload" id="upload" enctype="multipart/form-data">
-            Owner Id: <br><br> <input type="text" name="owner_id"><br><br>
-            ชื่ออัลบั้ม <br><br> <input type="text" name="name" placeholder="Please provide album name"><br><br>
-            <input type="file" name="file[]" multiple><br><br>
-            <button type="submit" class="btn btn-primary" >อัปโหลด</button>
-        </form>
-        </div>
-        <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-dismiss="modal">ปิด</button>
-        </div>
-        </div>
-    </div>
-    </div>
-    <!-- End Modal -->
-
-    </body>
+</body>
 </html>
-
-<script>
-    var form = document.getElementById("upload");
-    var request = new XMLHttpRequest();
-
-
-    function form.addEventListener('submit',function(e)){
-        e.preventDefault();
-        var formdata = new FormData(form);
-        request.open('post','/api/img/upload');
-        request.addEventListener("load",transferComplete);
-        request.send(formdata);
-    };
-
-    function transferComplete(data){
-        response = JSON.parse(data.currentTarget.response);
-        if(response.success){
-
-        }
-    }
-
-</script>
+@endsection
