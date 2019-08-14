bd2c5c33ff3cd6f6d4d6b2ed117d0c472c24a500
MukurowZ <zoz2542@gmail.com>
2019-08-15 03:48:13 +0700

    [EDIT] Navbar component

--- api/resources/js/components/NavbarComponent.vue
+++ api/resources/js/components/NavbarComponent.vue
@@ -1,14 +1,13 @@
 <template>
-    <div style="font-family: Kanit">
-  <b-navbar toggleable="lg" type="dark" variant="info">
-    <b-navbar-brand href="/">มดงานการเกษตร</b-navbar-brand>
+    <div style="font-family: Kanit; ">
+  <b-navbar toggleable="lg" type="light" style="background-color: #c4e6d2">
+    <b-navbar-brand href="/" style="font-weight: 450;">มดงานการเกษตร</b-navbar-brand>
 
     <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
     <b-collapse id="nav-collapse" is-nav>
       <b-navbar-nav>
-        <b-nav-item href="#">Link</b-nav-item>
-        <b-nav-item href="#" disabled>Disabled</b-nav-item>
+        <b-nav-item href="/">หน้าหลัก</b-nav-item>
         <b-nav-item-dropdown text="สินค้า" left>
           <b-dropdown-item href="#">--</b-dropdown-item>
           <b-dropdown-item href="#">ES</b-dropdown-item>
@@ -21,6 +20,7 @@
           <b-dropdown-item href="#">RU</b-dropdown-item>
           <b-dropdown-item href="#">FA</b-dropdown-item>
         </b-nav-item-dropdown>
+        <b-nav-item href="/">ติดต่อเรา</b-nav-item>
 
 
       </b-navbar-nav>
@@ -28,8 +28,8 @@
       <!-- Right aligned nav items -->
       <b-navbar-nav class="ml-auto">
         <b-nav-form>
-          <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
-          <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
+          <b-form-input size="sm" class="mr-sm-2" placeholder="พิมพ์เพื่อค้นหา"></b-form-input>
+          <b-button size="sm" class="my-2 my-sm-0" type="submit">ค้นหา <font-awesome-icon :icon="['fas', 'search']" /></b-button>
         </b-nav-form>
 
         <b-nav-item-dropdown text="Lang" left>
@@ -53,6 +53,20 @@
 
 <script>
 export default {
-
+    data() {
+        return {
+            categories: [],
+            category:{
+                id: '',
+                name: '',
+                head: '',
+            }
+        }
+    },
+    methods: {
+        getAllSet() {
+            // axios.get("").then(response => this.setSetData(response.data));
+        },
+    },
 }
 </script>
