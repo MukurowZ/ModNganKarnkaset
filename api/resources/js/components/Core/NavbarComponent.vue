ea632b1295a2dd4e2bad8b49a3599d2c15289068
MukurowZ <zoz2542@gmail.com>
2019-08-19 02:31:05 +0700

    [EDIT] Navbar menu list

--- api/resources/js/components/Core/NavbarComponent.vue
+++ api/resources/js/components/Core/NavbarComponent.vue
@@ -8,16 +8,17 @@
     <b-collapse id="nav-collapse" is-nav>
       <b-navbar-nav>
         <b-nav-item href="/">หน้าหลัก <font-awesome-icon :icon="['fas', 'home']" /></b-nav-item>
-        <b-nav-item  href="/product">สินค้า/บริการ <font-awesome-icon :icon="['fas', 'store']" /></b-nav-item>
+        <b-nav-item  href="/product">สินค้า <font-awesome-icon :icon="['fas', 'shopping-bag']" /></b-nav-item>
+        <b-nav-item  href="/service">บริการ <font-awesome-icon :icon="['fas', 'hand-holding']" /></b-nav-item>
         <b-nav-item href="/contact">ติดต่อเรา <font-awesome-icon :icon="['fas', 'phone']" /> </b-nav-item>
       </b-navbar-nav>
 
       <!-- Right aligned nav items -->
       <b-navbar-nav class="ml-auto">
-        <b-nav-form>
+        <!-- <b-nav-form>
           <b-form-input size="sm" class="mr-sm-2" placeholder="พิมพ์เพื่อค้นหา"></b-form-input>
           <b-button size="sm" class="my-2 my-sm-0" type="submit">ค้นหา <font-awesome-icon :icon="['fas', 'search']" /></b-button>
-        </b-nav-form>
+        </b-nav-form> -->
 
         <b-nav-item-dropdown text="Lang" left>
           <b-dropdown-item href="#">EN</b-dropdown-item>
