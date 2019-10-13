6f709e4f476015bfea5e5bdc26a554c547901d7f
TEERANAI KONGTANARUG <61130500213@st.sit.kmutt.ac.th>
2019-10-14 00:30:33 +0700

    [FIX] ShowEvent CSS fix

--- api/resources/js/components/Event/ShowEventComponent.vue
+++ api/resources/js/components/Event/ShowEventComponent.vue
@@ -7,30 +7,28 @@
           <div class="card" style="width: 100%;">
             <ul class="list-group list-group-flush">
               <li v-for="event in events" v-bind:key="event.id" class="list-group-item forum-post">
-                <div class="column">
-                  <div class="row">
-                    <div class="col-md-3">
-                      <img v-lazy="fullPath(event.img_set_id)" height="200px" />
-                    </div>
-                    <div class="col-md-9">
-                      <h4 class="card-title mb-2">{{event.event_name}}</h4>
-                      <p class="card-text">
-                        {{event.event_description}}
-                        <a
-                          style="color: gray"
-                          :href="getEventUrl(event.id)"
-                        >อ่านต่อ</a>
-                      </p>
-                    </div>
-                    <div class="info-box">
-                      <h6>
-                        {{event.owner_name}}
-                        <br />
-                        <small>Created on {{event.created_at}}</small>
-                        <br />
-                        <small>Last Updated {{event.updated_at}}</small>
-                      </h6>
-                    </div>
+                <div class="row" style="height: 90%">
+                  <div class="col-md-3">
+                    <img v-lazy="fullPath(event.img_set_id)" height="200px" />
+                  </div>
+                  <div class="col-md-6 event-info">
+                    <h4 class="card-title mb-2">{{event.event_name}}</h4>
+                    <p class="card-text">
+                      {{event.event_description}}
+                      <a
+                        style="color: gray"
+                        :href="getEventUrl(event.id)"
+                      >อ่านต่อ</a>
+                    </p>
+                  </div>
+                  <div class="info-box col-md-3">
+                    <h6>
+                      {{event.owner_name}}
+                      <br />
+                      <small>Created on {{event.created_at}}</small>
+                      <br />
+                      <small>Last Updated {{event.updated_at}}</small>
+                    </h6>
                   </div>
                 </div>
               </li>
@@ -43,9 +41,9 @@
 </template>
 
 <script>
-import { itemTypeMixin } from '../mixins/itemType.js'
+import { itemTypeMixin } from "../mixins/itemType.js";
 export default {
-    mixins: [itemTypeMixin],
+  mixins: [itemTypeMixin],
   mounted() {
     this.getEventData();
     this.getTHname();
@@ -53,7 +51,7 @@ export default {
   methods: {
     getEventData() {
       axios
-        .get("/api/"+this.itemType+"/")
+        .get("/api/" + this.itemType + "/")
         .then(response => this.setEventData(response.data));
     },
     setEventData(e) {
@@ -61,11 +59,12 @@ export default {
         axios.get("/api/img/set/" + a.img_set_id).then(response => {
           a.img_set_id = response.data.path;
         });
-        if(this.itemType=="event")
-            a.event_description = a.event_description.substring(0, 300) + "...";
-        else{
-            a.event_description = a.activity_description.substring(0, 300) + "...";
-            a.activity_description=null;
+        if (this.itemType == "event")
+          a.event_description = a.event_description.substring(0, 300) + "...";
+        else {
+          a.event_description =
+            a.activity_description.substring(0, 300) + "...";
+          a.activity_description = null;
         }
         a.owner_name = a.contentDetail[0].ownerName[0].name;
       });
@@ -79,31 +78,31 @@ export default {
     },
     getEventUrl(e) {
       if (e != null) {
-        return "/"+this.itemType+"/" + e;
+        return "/" + this.itemType + "/" + e;
       }
       return "";
     },
-    getTHname(){
-        if(this.itemType=="event"){
-            this.thname = "กิจกรรม"
-        }else this.thname = "ผลงาน"
+    getTHname() {
+      if (this.itemType == "event") {
+        this.thname = "กิจกรรม";
+      } else this.thname = "ผลงาน";
     }
   },
   data() {
     return {
-        thname: '',
-        events: [],
-        event: {
-            id: "0",
-            event_name: "",
-            event_description: "",
-            img_set_id: "",
-            created_at: "",
-            update_at: "",
-            img_path: "",
-            owner_name: ""
-        },
-        path: []
+      thname: "",
+      events: [],
+      event: {
+        id: "0",
+        event_name: "",
+        event_description: "",
+        img_set_id: "",
+        created_at: "",
+        update_at: "",
+        img_path: "",
+        owner_name: ""
+      },
+      path: []
     };
   }
 };
