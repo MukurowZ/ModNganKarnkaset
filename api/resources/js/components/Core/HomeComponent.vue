e23bc660be3e41295d2cd450d4c24d239c4431ac
JuicyRibs <53170057+JuicyRibs@users.noreply.github.com>
2019-11-12 23:19:26 +0700

    [UPDATE] Recent product section

--- api/resources/js/components/Core/HomeComponent.vue
+++ api/resources/js/components/Core/HomeComponent.vue
@@ -1,153 +1,217 @@
 <template>
-  <div>
-    <section class="cid-rBPYu8gBku mbr-fullscreen mbr-parallax-background" id="header2-5">
-      <div class="mbr-overlay" style="opacity: 0.5; background-color: rgb(35, 35, 35);"></div>
+    <div>
+        <section
+            class="cid-rBPYu8gBku mbr-fullscreen mbr-parallax-background"
+            id="header2-5"
+        >
+            <div
+                class="mbr-overlay"
+                style="opacity: 0.5; background-color: rgb(35, 35, 35);"
+            ></div>
 
-      <div class="container align-center">
-        <div class="row justify-content-md-center">
-          <div class="mbr-white col-md-10">
-            <h1 class="mbr-section-title mbr-bold pb-3 mbr-fonts-style display-1">มดงานการเกษตร</h1>
+            <div class="container align-center">
+                <div class="row justify-content-md-center">
+                    <div class="mbr-white col-md-10">
+                        <h1
+                            class="mbr-section-title mbr-bold pb-3 mbr-fonts-style display-1"
+                        >
+                            มดงานการเกษตร
+                        </h1>
 
-            <p
-              class="mbr-text pb-3 mbr-fonts-style display-5"
-            >คำเปรยคำเปรยคำเปรยคำเปรยคำเปรยคำเปรยคำเปรยคำเปรยคำเปรยคำเปรยคำเปรยคำเปรยคำเปรยคำเปรยคำเปรยคำเปรยคำเปรยคำเปรยคำเปรยคำเปรยคำเปรยคำเปรยคำเปรยคำเปรยคำเปรยคำเปรยคำเปรยคำเปรยคำเปรยคำเปรยคำเปรยคำเปรย</p>
-            <div class="mbr-section-btn">
-              <a class="btn btn-md btn-primary display-7" href="index.html#info3-c">
-                สินค้า
-                <br />
-              </a>
-              <a class="btn btn-md btn-secondary display-7" href="index.html#info3-h">บริการ</a>
+                        <p class="mbr-text pb-3 mbr-fonts-style display-5">
+                            คำเปรยคำเปรยคำเปรยคำเปรยคำเปรยคำเปรยคำเปรยคำเปรยคำเปรยคำเปรยคำเปรยคำเปรยคำเปรยคำเปรยคำเปรยคำเปรยคำเปรยคำเปรยคำเปรยคำเปรยคำเปรยคำเปรยคำเปรยคำเปรยคำเปรยคำเปรยคำเปรยคำเปรยคำเปรยคำเปรยคำเปรยคำเปรย
+                        </p>
+                        <div class="mbr-section-btn">
+                            <a
+                                class="btn btn-md btn-primary display-7"
+                                href="index.html#info3-c"
+                            >
+                                สินค้า
+                                <br />
+                            </a>
+                            <a
+                                class="btn btn-md btn-secondary display-7"
+                                href="index.html#info3-h"
+                                >บริการ</a
+                            >
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
-        </div>
-      </div>
-    </section>
+        </section>
 
-    <section class="engine">
-      <a href="https://mobirise.info/h">create your own web page for free</a>
-    </section>
-    <section class="mbr-section content4 cid-rCl7v3JI8y" id="content4-i">
-      <div class="container">
-        <div class="media-container-row">
-          <div class="title col-12 col-md-8">
-            <h2
-              class="align-center pb-3 mbr-fonts-style display-2"
-            >สินค้าและบริการทางการเกษตรครบวงจร</h2>
-            <h3
-              class="mbr-section-subtitle align-center mbr-light mbr-fonts-style display-5"
-            >อธิบายอธิบายอธิบายอธิบายอธิบายอธิบายอธิบาย</h3>
-          </div>
-        </div>
-      </div>
-    </section>
-
-    <section class="mbr-section info3 cid-rCl7oOAK9i mbr-parallax-background" id="info3-h">
-      <div class="mbr-overlay" style="opacity: 0.5; background-color: rgb(35, 35, 35);"></div>
-
-      <div class="container">
-        <div class="row justify-content-center">
-          <div class="media-container-column title col-12 col-md-10">
-            <h2 class="align-left mbr-bold mbr-white pb-3 mbr-fonts-style display-2">บริการ</h2>
-            <h3
-              class="mbr-section-subtitle align-left mbr-light mbr-white pb-3 mbr-fonts-style display-5"
-            >อธิบายอธิบายอธิบาย</h3>
-
-            <div class="mbr-section-btn align-left py-4">
-              <a v-on:click="getUrlService()" class="btn btn-primary display-4">ชมบริการทั้งหมด</a>
+        <section class="engine">
+            <a href="https://mobirise.info/h"
+                >create your own web page for free</a
+            >
+        </section>
+        <section class="mbr-section content4 cid-rCl7v3JI8y" id="content4-i">
+            <div class="container">
+                <div class="media-container-row">
+                    <div class="title col-12 col-md-8">
+                        <h2 class="align-center pb-3 mbr-fonts-style display-2">
+                            สินค้าและบริการทางการเกษตรครบวงจร
+                        </h2>
+                        <h3
+                            class="mbr-section-subtitle align-center mbr-light mbr-fonts-style display-5"
+                        >
+                            อธิบายอธิบายอธิบายอธิบายอธิบายอธิบายอธิบาย
+                        </h3>
+                    </div>
+                </div>
             </div>
-          </div>
-        </div>
-      </div>
-    </section>
+        </section>
 
-    <section class="services1 cid-rCl6JgpQ7y" id="services1-f">
-      <!---->
+        <section
+            class="mbr-section info3 cid-rCl7oOAK9i mbr-parallax-background"
+            id="info3-h"
+        >
+            <div
+                class="mbr-overlay"
+                style="opacity: 0.5; background-color: rgb(35, 35, 35);"
+            ></div>
 
-      <!---->
-      <!--Overlay-->
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="media-container-column title col-12 col-md-10">
+                        <h2
+                            class="align-left mbr-bold mbr-white pb-3 mbr-fonts-style display-2"
+                        >
+                            บริการ
+                        </h2>
+                        <h3
+                            class="mbr-section-subtitle align-left mbr-light mbr-white pb-3 mbr-fonts-style display-5"
+                        >
+                            อธิบายอธิบายอธิบาย
+                        </h3>
 
-      <!--Container-->
-      <div class="container">
-        <div class="row justify-content-center">
-          <!--Titles-->
-          <div class="title pb-5 col-12">
-            <h2 class="align-left pb-3 mbr-fonts-style display-1">บริการล่าสุด</h2>
-          </div>
-
-          <three-product-component :itemType="'service'"></three-product-component>
-
-        </div>
-      </div>
-    </section>
-
-    <section class="mbr-section info3 cid-rCl5iV11MD mbr-parallax-background" id="info3-c">
-      <div class="mbr-overlay" style="opacity: 0.5; background-color: rgb(35, 35, 35);"></div>
-
-      <div class="container">
-        <div class="row justify-content-center">
-          <div class="media-container-column title col-12 col-md-10">
-            <h2 class="align-left mbr-bold mbr-white pb-3 mbr-fonts-style display-2">สินค้า</h2>
-            <h3
-              class="mbr-section-subtitle align-left mbr-light mbr-white pb-3 mbr-fonts-style display-5"
-            >Shape your future web project with sharp design and refine coded functions</h3>
-
-            <div class="mbr-section-btn align-left py-4">
-              <a v-on:click="getUrlProduct()" class="btn btn-primary display-4">ชมสินค้าทั้งหมด</a>
+                        <div class="mbr-section-btn align-left py-4">
+                            <a
+                                v-on:click="getUrlService()"
+                                class="btn btn-primary display-4"
+                                >ชมบริการทั้งหมด</a
+                            >
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
-        </div>
-      </div>
-    </section>
+        </section>
 
-    <section class="services1 cid-rCl5TV8PlF" id="services1-d">
-      <!---->
+        <section class="services1 cid-rCl6JgpQ7y" id="services1-f">
+            <!---->
 
-      <!---->
-      <!--Overlay-->
+            <!---->
+            <!--Overlay-->
 
-      <!--Container-->
-      <div class="container">
+            <!--Container-->
+            <div class="container">
+                <div class="row justify-content-center">
+                    <!--Titles-->
+                    <div class="title pb-5 col-12">
+                        <h2 class="align-left pb-3 mbr-fonts-style display-1">
+                            บริการล่าสุด
+                        </h2>
+                    </div>
+
+                    <three-product-component
+                        :itemType="'service'"
+                    ></three-product-component>
+                </div>
+            </div>
+        </section>
+
+        <section
+            class="mbr-section info3 cid-rCl5iV11MD mbr-parallax-background"
+            id="info3-c"
+        >
+            <div
+                class="mbr-overlay"
+                style="opacity: 0.5; background-color: rgb(35, 35, 35);"
+            ></div>
+
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="media-container-column title col-12 col-md-10">
+                        <h2
+                            class="align-left mbr-bold mbr-white pb-3 mbr-fonts-style display-2"
+                        >
+                            สินค้า
+                        </h2>
+                        <h3
+                            class="mbr-section-subtitle align-left mbr-light mbr-white pb-3 mbr-fonts-style display-5"
+                        >
+                            Shape your future web project with sharp design and
+                            refine coded functions
+                        </h3>
+
+                        <div class="mbr-section-btn align-left py-4">
+                            <a
+                                v-on:click="getUrlProduct()"
+                                class="btn btn-primary display-4"
+                                >ชมสินค้าทั้งหมด</a
+                            >
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="services1 cid-rCl5TV8PlF" id="services1-d">
+            <!---->
+
+            <!---->
+            <!--Overlay-->
+
+            <!--Container-->
+            <div class="container">
+                <div class="row justify-content-center">
+                    <!--Titles-->
+                    <div class="title pb-5 col-12">
+                        <h2 class="align-left pb-3 mbr-fonts-style display-1">
+                            สินค้าล่าสุด
+                            <br />
+                        </h2>
+                    </div>
+
+                    <three-product-component
+                        :itemType="'product'"
+                    ></three-product-component>
+                </div>
+            </div>
+        </section>
+        <section>
             <div class="row justify-content-center">
-            <!--Titles-->
-            <div class="title pb-5 col-12">
-                <h2 class="align-left pb-3 mbr-fonts-style display-1">
-                สินค้าล่าสุด
-                <br />
-                </h2>
+                <table>
+                    <tr>
+                        <td style="width: 50%; padding-left: 4em">
+                            <h1>New event from us</h1>
+                        </td>
+                        <td style="width: 50%; padding-left: 4em">
+                            <h1>New activity from us</h1>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <three-event-component
+                                :itemType="'event'"
+                            ></three-event-component>
+                        </td>
+                        <td>
+                            <three-event-component
+                                :itemType="'activity'"
+                            ></three-event-component>
+                        </td>
+                    </tr>
+                </table>
             </div>
-
-            <three-product-component :itemType="'product'"></three-product-component>
-
-        </div>
-      </div>
-    </section>
-        <div class="row justify-content-center">
-            <table>
-            <tr>
-                <td style="width: 50%; padding-left: 4em"><h1>New event from us</h1></td>
-                <td style="width: 50%; padding-left: 4em"><h1>New activity from us</h1></td>
-            </tr>
-            <tr>
-                <td>
-                    <three-event-component :itemType="'event'"></three-event-component>
-                </td>
-                <td>
-                    <three-event-component :itemType="'activity'"></three-event-component>
-                </td>
-            </tr>
-            </table>
-        </div>
-
-
-
-
-  </div>
+        </section>
+    </div>
 </template>
 
 <script>
 export default {
     mounted() {
-        history.pushState('', '', '/');
+        history.pushState("", "", "/");
     },
     methods: {
         getUrlProduct() {
@@ -157,7 +221,7 @@ export default {
         getUrlService() {
             window.location.href = "/service";
             return "";
-        },
+        }
     }
 };
 </script>
