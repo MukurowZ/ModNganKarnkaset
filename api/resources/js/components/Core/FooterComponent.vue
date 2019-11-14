edad8badb23535be292f3eddd418ee8c6ee3812b
Mukurow <zoz2542@gmail.com>
2019-11-14 14:32:15 +0700

    [MERGE]

--- api/resources/js/components/Core/FooterComponent.vue
+++ api/resources/js/components/Core/FooterComponent.vue
@@ -1,33 +1,4 @@
 <template>
-<<<<<<< Updated upstream
-    <footer
-        class="pt-5"
-        style="font-family: Kanit; 
-        background-image: URL('/assets/images/footer.jpg'); height: 15rem;"
-        >
-        <div class="container">
-            <div class="row" style="color: white;">
-                <div class="company-info col-sm">
-                    <h3>ModNganKarnkaset Thai</h3>
-                    <p>
-                        Something about company. Something about comp omething
-                        about company. Something about company. Something about
-                        company. Something about company. Something about
-                        company. Something about company.
-                    </p>
-                </div>
-                <div class="col-sm"></div>
-                <div class="company-contact col-sm">
-                    <h3>Contact Us</h3>
-                    <p>
-                        Something about company. Something about comp omething
-                        about company. Something about company. Something about
-                        company. Something about company. Something about
-                        company. Something about Company.
-                    </p>
-                </div>
-            </div>
-=======
   <footer
     class="py-5"
     style="color: #000000; font-family: Kanit; background-color: #c4e6d2; background-image: url(./Image/footer.jpg); min-height: 15rem;">
@@ -39,7 +10,6 @@
             Something about company. Something about comp omething about company. Something about company.
             Something about company. Something about company. Something about company. Something about company.
           </p>
->>>>>>> Stashed changes
         </div>
     </footer>
 </template>
