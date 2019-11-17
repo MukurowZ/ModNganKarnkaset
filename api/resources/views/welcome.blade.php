5116384f4171b844d095fa0f8a3b25de750cb184
MukurowZ <zoz2542@gmail.com>
2019-11-18 00:52:30 +0700

    [REMOVE] remove unwanted import

--- api/resources/views/welcome.blade.php
+++ api/resources/views/welcome.blade.php
@@ -2,13 +2,7 @@
 @section('title','มดงานการเกษตร - หน้าหลัก')
 
 @section('content')
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
     <!-- <script>window.Laravel = { csrfToken: '{{ csrf_token() }}'}</script> -->
 
     <!-- Fonts -->
@@ -126,21 +120,6 @@
     <script src="/assets/theme/js/script.js"></script>
     <script src="{{ asset('js/app.js') }}"></script>
     <script type="text/javascript" src="js/app.js"></script>
-    <!-- <div class="flex-center position-ref full-height">
-        @if (Route::has('login'))
-        <div class="top-right links">
-            @auth
-            <a href="{{ url('/home') }}">Home</a>
-            @else
-            <a href="{{ route('login') }}">Login</a>
-
-            @if (Route::has('register'))
-            <a href="{{ route('register') }}">Register</a>
-            @endif
-            @endauth
-        </div>
-        @endif
-    </div> -->
 </body>
 
 </html>
