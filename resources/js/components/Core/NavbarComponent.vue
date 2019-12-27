145160713d849876da66842da67ba04f3dbeddd1
JuicyRibs <moji_001122@hotmail.com>
2019-12-27 13:35:15 +0700

    [FIX] Dropdown menu overflow

--- resources/js/components/Core/NavbarComponent.vue
+++ resources/js/components/Core/NavbarComponent.vue
@@ -31,7 +31,7 @@
             <v-avatar color="teal" size="48">
                 <span class="white--text headline">EEE</span>
             </v-avatar>
-            <b-nav-item-dropdown v-if="auth" text="จัดการ" v-on:click="logout">
+            <b-nav-item-dropdown v-if="auth" text="จัดการ" v-on:click="logout" class="mr-4">
                 <b-dropdown-item v-on:click="url('event')">กิจกรรม</b-dropdown-item>
                 <b-dropdown-item v-on:click="url('activity')">ผลงาน</b-dropdown-item>
                 <b-dropdown-item v-on:click="url('product')">ผลิตภัณฑ์</b-dropdown-item>
